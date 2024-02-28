18da896b6667e790a9ffd6b29b00ce09d5af81b8
Amber7222 <159792501+Amber7222@users.noreply.github.com>
2024-02-28 12:00:39 +0000

    Add files via upload

---
+++ 2.R
@@ -0,0 +1,14 @@
+
+# Question 2 
+
+# installing and loading packages 
+library(dplyr)  # for data manipulation
+library(ggplot2)  # for visualization
+library(pROC)  # for ROC curve and AUC calculation
+library(caret)  # for model evaluation
+library(MASS)  # for stepAIC
+
+# setting the working directory and loading csv file
+setwd("C:\\Users\\qosja\\Downloads\\")
+read.csv("recommendations.csv")
+data <- read.csv("recommendations.csv")
